d1cd6a009cb554c6e719026b727abc5ade423b08
vijay <srinivasrao683@gmail.com>
2020-08-30 16:05:06 +0530

    first commit

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -101,7 +101,13 @@ DATABASES = {
 
     }
 }
-
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 
 # Password validation
